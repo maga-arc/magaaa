a2b77fc5bd5f0372a450e64a6291ab477f026f56
maga-arc <magamedresidov@gmail.com>
2026-07-20 16:42:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = '8646830704:AAERVK1qf0cr_wN9Y4ZwpJ-gFyg8dWNO90I'
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Здраствуйте")
-
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT, hello))
-app.run_polling()
